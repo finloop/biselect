5689208bd32617abde515c4e738aabad403cad89
Piotr Krawiec <piotr.krawiec23@gmail.com>
2019-10-11 13:18:00 +0200

    Added few comments

--- main.c
+++ main.c
@@ -2,7 +2,9 @@
 #include <stdlib.h>
 #include <math.h>
 
+// Definicja prototypu funkcji na której operuje algorytm.
 typedef double( * fun ) (double);
+
 double bsin(double);
 
 double bisection_with_precision(double x1, double x2, fun getval);
@@ -16,10 +18,16 @@ int main()
     return 0;
 }
 
+// Jedna z funkcji na której może działać algorytm.
 double bsin(double x){
     return sin(x);
 }
 
+// Algorytm bisekcji.
+// x1 lewa - granica przedziału
+// x2 prawa - granica przedziału
+// getval - funkcja na której operuje algorytm
+// Przykład: double root = bisection_with_precision(M_PI_2,13/10*M_PI , bsin);
 double bisection_with_precision(double x1, double x2, fun getval)
 {
     // 0.0001 to precyzja jej zwiększenie to
