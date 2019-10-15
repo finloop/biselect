969e4c41f5649e8b51081c7cf0ed5fb08c5d1560
finloop <piotr.krawiec23@gmail.com>
2019-10-15 14:04:32 +0200

    added plots

--- main.c
+++ main.c
@@ -7,13 +7,13 @@ typedef double( * fun ) (double);
 
 double bsin(double);
 
-double bisection_with_precision(double x1, double x2, fun getval);
+double bisection_with_precision(double x1, double x2, fun getval, double precision);
 
 int main()
 {
     double x1 = M_PI_2;
     double x2 = 13/10*M_PI;
-    double r = bisection_with_precision(x1,x2 , bsin);
+    double r = bisection_with_precision(x1,x2 , bsin, 0.1);
     printf("Done: %f\n", r);
     return 0;
 }
@@ -23,7 +23,6 @@ double bsin(double x){
     return sin(x);
 }
 
-
 // Algorytm bisekcji.
 // Parametry:
 // x1 lewa granica przedziału
@@ -31,22 +30,23 @@ double bsin(double x){
 // getval funkcja na której operuje algorytm (dowolna funkcja która zwraca double
 // przyjmuje jeden argument double)
 // Przykład: double root = bisection_with_precision(M_PI_2,13/10*M_PI , bsin);
-double bisection_with_precision(double x1, double x2, fun getval)
+double bisection_with_precision(double x1, double x2, fun getval, double precision)
 {
     // Sprawdzenie czy osiągneliśmy wymaganą precyzję
-    while(fabs(x1-x2) >= 0.0001)
+    printf("  %-10s%-10s%-10s%-10s\n", "Iteracja", "L", "P", "Precyzja");
+    int i = 0;
+    printf("  %-10d%-10f%-10f%-10f\n", i, x1, x2, fabs(x1-x2));
+    while(fabs(x1-x2) >= precision)
     {
-        printf("Working on: x1: %f, x2: %f\n", x1, x2);
-        printf("Current precision: %f \n", fabs(x1-x2));
         // Obliczenie warości funkcji dla granic i środka przedziału
         double v_x1 = getval(x1);
         double s = (x1+x2)/2.0;
         double v_s = getval(s);
 
-        // Sprawdzam czy iloczyn lewej granicy i środka jest różnych znaków 
+        // Sprawdzam czy iloczyn lewej granicy i środka jest różnych znaków
         if(v_x1*v_s > 0)
-        {  
-            // Jeżeli jest takich samych znaków to znaczy, że miejsce zerowe 
+        {
+            // Jeżeli jest takich samych znaków to znaczy, że miejsce zerowe
             // jest w [s,x2] zatem nadpisuję lewą granicę środkiem
             // przedziału
             x1 = s;
@@ -55,8 +55,8 @@ double bisection_with_precision(double x1, double x2, fun getval)
             // zatem nadpisuję prawą granicę środkiem przedziału
             x2 = s;
         }
+        i++;
+        printf("  %-10d%-10f%-10f%-10f\n", i, x1, x2, fabs(x1-x2));
     }
-    printf("Done: %f      %f\n", x1, x2);
-    printf("Precision: %f \n", fabs(x1-x2));
     return (x1+x2)/2.0;
 }
---
+++ sinx.m
@@ -0,0 +1,6 @@
+x = 0.1:0.05:5;
+y = sin(x);
+
+plot(x, y);
+hold on
+
